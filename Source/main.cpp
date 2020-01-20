f7210527bb36a36285793b9dc88ec1611a9d9165
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2020-01-19 19:01:44 -0600

    Added openGameTemplatesFile() to read in user file
    When the user is prompted to select a file to be parsed the openGameTemplates function will be called to take that path and open and read in the selected file.
    
    It will return a list of game template objects if they can be retrieved from the file. (Will return nullptr if it can not.)

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,31 @@
 #include <QApplication>
+#include <QDebug>
+#include <QTextStream>
+#include <QFile>
+
+QList<GameTemplate>* openGameTemplatesFile(QString directory) {
+    QFile templatesFile(directory);
+
+    if (templatesFile.open(QIODevice::ReadOnly) == false) {
+        qCritical("Output command failed. The supplied file path can not be opened.");
+        return nullptr;
+    }
+
+    QByteArray templatesFileBytes = templatesFile.readAll();
+
+    int offsetOfFileIdentifier = templatesFileBytes.indexOf("AULB");
+    if (offsetOfFileIdentifier == -1) {
+        qCritical("Output command failed. The supplied file does not contain the \"AULB\" file identifier.");
+        return nullptr;
+    }
+
+    if (offsetOfFileIdentifier != 0) {
+        // Remove any data prior to the file identifer.
+        templatesFileBytes.remove(0, offsetOfFileIdentifier);
+    }
+
+    return parseGameTemplatesFile(templatesFileBytes);
+}
 
 int main(int argc, char *argv[])
 {
